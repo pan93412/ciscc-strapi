8336a30d094dd0169028d8a8992e1369b3d9fb46
pan93412 <pan93412@gmail.com>
2021-07-31 03:17:17 +0800

    init(): released for ciscc-backend 2ff1425df92ed5fbb2832d878e272abeccbf1559

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'SPECIFY_YOUR_OWN_SECRET_IN_ENVVAR'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '510e08c0-cf13-42d6-b157-885631546188'
+};
